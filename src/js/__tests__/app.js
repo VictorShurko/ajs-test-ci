ec08dcf7f964e88d8e1d49a43949e22bca33e412
Victor Shurko <victorshurko@yandex.ru>
2024-11-14 16:33:48 +0300

    update app test

--- src/js/__tests__/app.js
+++ src/js/__tests__/app.js
@@ -1,13 +1,19 @@
 import userStatus from '../app.js';
 
 test('user health healthy test', () => {
-    expect(userStatus({name: 'Маг', health: 90})).toBe('healthy');
+    expect(userStatus({
+        name: 'Маг', health: 90
+    })).toBe('healthy');
 });
 
 test('user health wounded test', () => {
-    expect(userStatus({name: 'Маг', health: 50})).toBe('wounded');
+    expect(userStatus({
+        name: 'Маг', health: 50
+    })).toBe('wounded');
 });
 
 test('user health critical test', () => {
-    expect(userStatus({name: 'Маг', health: 15})).toBe('critical');
+    expect(userStatus({
+        name: 'Маг', health: 15
+    })).toBe('critical');
 });
